65cea56f30f969aba0c7950d7c71f2eda628d76f
Andrei Mihai <andreimihai2305is@gmail.com>
2026-06-13 09:49:35 +0300

    added MEM module implementation

---
+++ MEM.v
@@ -0,0 +1,34 @@
+//`include "parts/memory.v" It's already included in IF and
+// it sees it twice, causing a compilation error
+
+module MEM(
+    din,
+    mem_write,
+    dout,
+);
+
+    // Write reg   = 101:97;
+    // Zero        = 96;
+    // New PC      = 95:64;
+    // ALU result  = 63:32;
+    // Read data 2 = 31: 0;
+    input [101:0]  din;
+
+    // Write reg  = 68:64;
+    // Read Data  = 63:32;
+    // ALU Result = 31:0;
+    output [68:0] dout;
+
+    input mem_write;
+
+    memory data_mem(
+       .addr_in(din[63:32]),
+       .mem_write(mem_write),
+       .data_in(din[31:0]),
+       .data_out(dout[63:32])
+    );
+
+    assign dout[63:32] = din[63:32];
+    assign dout[68:64] = din[101:97];
+
+endmodule
